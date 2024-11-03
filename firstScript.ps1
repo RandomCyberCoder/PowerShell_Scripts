fb4022b5cca937ea5eedbc1075f63e5b595cf4f3
RandomCyberCoder <emmanueldelgado74@gmail.com>
2024-11-02 18:22:36 -0700

    initial scripts

---
+++ firstScript.ps1
@@ -0,0 +1,38 @@
+﻿param(
+    [int]$Count = 10,
+    [Parameter(
+        Mandatory)]
+    [string]$fileLoc
+)
+
+function pingNumTimes{
+    param(
+        [int]$Count,
+        [Parameter(
+            Mandatory)]
+        [string]$fileLoc
+    )
+    
+    Write-Host "Running ping test $Count times and storing"
+        " The results at $fileLoc"
+    ping 8.8.8.8 -n $Count > $fileLoc
+    Write-Host "Ping Test finished"
+
+}
+
+function pingCSV{
+    param(
+        [int]$Count,
+        [Parameter(
+            Mandatory)]
+        [string]$fileLoc
+    )
+    $job = ping 8.8.8.8 -n $Count
+    $job | Export-CSV $fileLoc
+}
+
+pingNumTimes $Count $fileLoc
+#pingCSV $Count $fileLoc
+
+
+
