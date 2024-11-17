7391f2b2f7d1dffced03ca264cf4f96d48c1de87
RandomCyberCoder <emmanueldelgado74@gmail.com>
2024-11-17 00:25:35 -0800

    updates

--- record_iPerf.ps1
+++ record_iPerf.ps1
@@ -2,10 +2,14 @@
     [Parameter(
         Mandatory)]
     [string]$serverIp,
+    [string]$serverPort,
+    [string]$iPerfPath,
     [string]$saveResHere
 )
-
+$captureResults = ""
 $keyExists = $PSBoundParameters.ContainsKey("saveResHere")
+$portExists = $PSBoundParameters.ContainsKey("serverPort")
+$iperfExeExists = $PSBoundParameters.ContainsKey("iPerfPath")
 
 #if no file given to save results create a file path
 if(!($keyExists)){
@@ -15,10 +19,23 @@ if(!($keyExists)){
 
 #path to the iperf3 executable
 $iPerfExePath = "C:\Users\vboxuser\Downloads\iperf3.17_64\iperf3.17_64\iperf3.exe"
+if(!($iperfExeExists)){
+    $iPerfExePath = $iPerfPath
+}
+else{
+    $iPerfExePath = "iperf3.exe"
+}
+
 
 #exectue the iperf file as the client and talk to the server with
 #ip address $serverIp. Output the results in JSON fomrat
-$captureResults = & $iPerfExePath -c $serverIp -J
+if($keyExists){
+    $captureResults = & $iPerfExePath -c $serverIp -p $serverPort -J
+}
+else{
+    $captureResults = & $iPerfExePath -c $serverIp -J
+}
+
 #bottom thing sorta does same as above but asks for admin privelages
 #Start-Process -FilePath $iPerfExePath -ArgumentList "-V -c $serverIp"
 
@@ -27,6 +44,12 @@ $captureResults = & $iPerfExePath -c $serverIp -J
 #Write-Output $captureResults
 
 Set-Content -Path $saveResHere -Value $captureResults
+
+
+#run python executable
+#.\pythonExectuableName $saveResHere
+
+
 
 #todo
 #parse the json to create a new json object
